ce9064abe560c47500a75b16b75b0e207839fcdb
Péter Marcell <pmarcell.1021@gmail.com>
2025-08-16 12:33:49 +0200

    Add full CRUD endpoints

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from typing import List
 from schemas import Note, NoteCreate
 
@@ -19,3 +19,25 @@ def create_note(note: NoteCreate):
     notes_db.append(new_note)
     return new_note
 
+@router.get("/noted/{note_id}", response_model=Note)
+def get_note(note_id: int):
+    for note in notes_db:
+        if note.id == note_id:
+            return note
+    raise HTTPException(status_code=404, detail="Note not found")
+
+@router.put("/notes/{note_id}", response_model=Note)
+def update_note(note_id: int, updated_note: NoteCreate):
+    for index, note in enumerate(notes_db):
+        if note.id == note_id:
+            notes_db[index] = Note(id=note_id, **updated_note.model_dump())
+            return notes_db[index]
+    raise HTTPException(status_code=404, detail="Note not found")
+
+@router.delete("/notes/{note_id}")
+def delete_note(note_id: int):
+    for index, note in enumerate(notes_db):
+        if note.id == note_id:
+            deleted_note = notes_db.pop(index)
+            return {"message": "Note deleted", "note": delete_note}
+    raise HTTPException(status_code=404, detail="Note not found")
